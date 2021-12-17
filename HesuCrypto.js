e5dbfbf220cb296bff81ee6dce42ea4116cce440
mart-anthony-stark <martsalazar0@gmail.com>
2021-12-17 13:18:18 +0800

    Test validation

--- HesuCrypto.js
+++ HesuCrypto.js
@@ -6,3 +6,5 @@ HesuCrypto.addBlock(new Block(1, "12/16/2021", { amount: 10 }));
 HesuCrypto.addBlock(new Block(1, "12/16/2021", { amount: 8 }));
 
 console.log(JSON.stringify(HesuCrypto, null, 4));
+// HesuCrypto.chain[1].data = { amount: 100 };
+console.log("Is chain valid?", HesuCrypto.isValid());
