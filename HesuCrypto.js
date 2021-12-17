7db162956f025b411fa414e3837e17866e7ca5c2
mart-anthony-stark <martsalazar0@gmail.com>
2021-12-17 21:14:02 +0800

    Add mining implementation

--- HesuCrypto.js
+++ HesuCrypto.js
@@ -2,10 +2,18 @@ const Blockchain = require("./Blockchain");
 const Block = require("./block");
 
 let HesuCrypto = new Blockchain();
+
+console.log("Mining....");
 HesuCrypto.addBlock(new Block(1, "12/16/2021", { amount: 10 }));
+
+console.log("Mining Block...");
 HesuCrypto.addBlock(new Block(2, "12/16/2021", { amount: 8 }));
 
+console.log("Mining....");
+HesuCrypto.addBlock(new Block(3, "12/16/2021", { amount: 10 }));
+
+console.log("Mining Block...");
+HesuCrypto.addBlock(new Block(4, "12/16/2021", { amount: 8 }));
+
 console.log(JSON.stringify(HesuCrypto, null, 4));
-// HesuCrypto.chain[1].data = { amount: 100 };
-// HesuCrypto.chain[1].hash = HesuCrypto.chain[1].generateHash();
-console.log("Is blockchain valid?", HesuCrypto.isValid());
+// console.log("Is blockchain valid?", HesuCrypto.isValid());
