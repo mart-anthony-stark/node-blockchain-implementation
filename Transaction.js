80d14185d74d5abb5b35282833909d8f647f80ab
mart-anthony-stark <martsalazar0@gmail.com>
2021-12-18 15:56:47 +0800

    Add transaction class

---
+++ Transaction.js
@@ -0,0 +1,8 @@
+class Transaction {
+  constructor(fromAddress, toAddress, amount) {
+    this.fromAddress = fromAddress;
+    this.toAddress = toAddress;
+    this.amount = amount;
+  }
+}
+module.exports = Transaction;
